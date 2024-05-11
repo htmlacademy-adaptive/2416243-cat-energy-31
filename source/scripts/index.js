18d7023911da2779da8b0ae766062fdff85ea070
Sergei Pozdeev <122926581+reking06@users.noreply.github.com>
2024-05-11 11:18:23 -0400

    responsive grids of small blocks (#8)
    * responsive grids of small blocks
    
    * changes to styles and html files; map added
    
    * map js, scss added to vendor; some minor fixes to html, scss, js files
    
    * some changes to styles for hero, results blocks
    
    ---------
    
    Co-authored-by: Sergei <red00009@outlook.com>

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,13 +1,31 @@
-const buttonMenu = document.querySelector('.menu-button');
-const listMenu = document.querySelector('.navigation__list');
+import './../vendor/leaflet/leaflet.js';
+
+const buttonMenu = document.querySelector('.main-header__toggle');
+const navMenu = document.querySelector('.navigation');
 const slider = document.querySelector('.slider__wrapper');
 const beforeImage = document.querySelector('.slider__item--before');
 const afterImage = document.querySelector('.slider__item--after');
 const sliderLine = document.querySelector('.slider__line');
+/* eslint-disable */
+const mapLocation = L.map('site-map').setView([59.9383496968133, 30.322116673359545], 18);
+const myIcon = L.icon({className: 'footer__map-icon', iconUrl: 'images/map-pin@1x.png'});
+const locationMarker = L.marker([59.93878233419091, 30.32281404770163], {icon: myIcon}).addTo(mapLocation);
+const mapLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 20,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapLocation);
+/* eslint-enable */
+
+navMenu.classList.remove('navigation--nojs');
 
 buttonMenu.addEventListener('click', () => {
-  listMenu.classList.toggle('navigation__list--opened');
-  buttonMenu.classList.toggle('menu-button--opened');
+  if (navMenu.classList.contains('navigation--closed')) {
+    navMenu.classList.remove('navigation--closed');
+    navMenu.classList.add('navigation--opened');
+  } else {
+    navMenu.classList.remove('navigation--opened');
+    navMenu.classList.add('navigation--closed');
+  }
 });
 
 function sliderMouseMove(event) {
